bca9c681b514540acc801c11860149f84d930a50
rksrohitks <rohitks2610@gmail.com>
2025-05-29 12:31:57 +0000

    msg

--- reactapp/src/Components/PrivateRoute.jsx
+++ reactapp/src/Components/PrivateRoute.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import {Route, Redirect} from 'react-router-dom';
 import {useSelector } from 'react-redux';
-const PrivateRoute = () => {
+const PrivateRoute = ({component: component, ...rest}) => {
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    //const  isAuthenticated = !!
     return(
         <Route
+        {...rest}
+        render = {props =>
+            isAuthenticated ? (
+                <Component {...props}/>
+            ):(
+                <Redirect to="/login" />
+            )
+        }
         
         />
     )
